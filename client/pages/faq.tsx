88220129bc3af41a921bccbd551cc16c81d348f3
James Ball <jamie.j.ball@gmail.com>
2022-07-10 13:12:02 -0700

    init FAQ page

---
+++ client/pages/faq.tsx
@@ -0,0 +1,53 @@
+import Nav from '../components/Nav';
+const Faq = () => {
+  return (
+    <>
+      <Nav />
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Focus me to see content
+        </div>
+        <div className="collapse-content">
+          <p>tabindex="0" attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Focus me to see content
+        </div>
+        <div className="collapse-content">
+          <p>tabindex="0" attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Focus me to see content
+        </div>
+        <div className="collapse-content">
+          <p>tabindex="0" attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          Focus me to see content
+        </div>
+        <div className="collapse-content">
+          <p>tabindex="0" attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Faq;
