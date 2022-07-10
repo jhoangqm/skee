6b312c8447a2830c52501459798d223c8ba7d545
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-09 21:02:17 -0400

    Removes unnecessary files

--- server/src/helpers/uploadMiddleware.js
+++ server/src/helpers/uploadMiddleware.js
@@ -1,16 +1,16 @@
 const multer = require('multer');
-// const path = require('path');
+const path = require('path');
 
 // This is the storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './src/tmp_storage'); // Replace 'upload' with destination (need to put DB)
+    cb(null, 'uploads'); // Replace 'upload' with destination (need to put DB)
   },
   filename: function (req, file, cb) {
     console.log('====================');
     console.log('file: ', file);
     console.log('====================');
-    cb(null, file.fieldname + '-' + Date.now());
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
@@ -18,7 +18,7 @@ const upload = multer({ storage: storage });
 
 // This is the upload function
 const singleUpload = (req, res, next) => {
-  const uploadSingle = upload.single('myFile');
+  const uploadSingle = upload.single('singleFile');
 
   uploadSingle(req, res, (err) => {
     if (err) {
