d317e0f562b46573fd1662581fc3880b4275a184
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-10 11:45:37 -0700

    adds carousel

---
+++ client/components/Carousel.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+const Carousel = () => {
+
+  return (
+    
+<div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://placeimg.com/250/180/arch" className="rounded-box" />
+  </div>
+</div>
+  )
+}
+
+export default Carousel
