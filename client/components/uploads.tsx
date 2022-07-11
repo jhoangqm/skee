71c928e6033c08a8fa1d52b7cdf5ff877656b00a
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-10 20:15:20 -0400

    Fixed upload, it sends data to a website ( Testing point )

--- client/components/uploads.tsx
+++ client/components/uploads.tsx
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from "react";
-const axios = require("axios");
+const Axios = require("axios");
 
 function Upload() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState();
 
-  useEffect(() => {})
+  const send = event => {
+    const data = new FormData();
+    data.append("name", name);
+    data.append("file", file);
+
+    Axios.post("https://httpbin.org/anything", data)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
 
   return (
     <div>
     <h1>File uploads</h1>
-    <form action="/upload" encType="multipart/form-data" method="POST"> 
-      <input type="file" name="files" multiple/>
-       <button className='btn' type="submit" value="Upload a file">Upload</button>
+    {/* <form action="/upload" encType="multipart/form-data" method="POST">  */}
+      <form action="#">
+      <input 
+      type="file" 
+      id='file'
+      // name="files" 
+      onChange={event => {
+        const file = event.target.files[0];
+        setFile(file)
+      }} 
+        multiple/>
+       <button 
+       className='btn' 
+       type="submit" 
+       onClick={send}>Upload</button>
     </form>
     </div>
   )
