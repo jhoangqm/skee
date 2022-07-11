e5d6fb6b57067b0aacfd9da44415a81bf2370b5f
James Ball <jamie.j.ball@gmail.com>
2022-07-11 12:58:23 -0700

    add pro create sign up

--- client/pages/api/pros/index.tsx
+++ client/pages/api/pros/index.tsx
@@ -1,8 +1,37 @@
 import { prisma } from '../../../db';
 
 // query function to find all pros
-export default async function handler(_req: any, res: any) {
-  const pros = await prisma.pros.findMany();
-  res.json(pros);
-  
+export default async function handler(req: any, res: any) {
+  const parsed = JSON.parse(req.body);
+  if (req.method === 'POST') {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      certBody,
+      level,
+      phoneNumber,
+      resortId,
+    } = parsed;
+    const pro = await prisma.pros.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password,
+        certBody,
+        bio: '',
+        level: Number(level),
+        phoneNumber,
+        resortId: Number(resortId),
+      },
+    });
+
+    res.status(200).json({ message: 'Pro created' });
+  }
+  if (req.method === 'GET') {
+    const pros = await prisma.pros.findMany();
+    res.json(pros);
+  }
 }
