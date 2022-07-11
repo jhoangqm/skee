2f58260cfc9dbb51530994507a67183726cf8536
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-11 12:12:16 -0700

    adds working modal

--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -26,6 +26,7 @@ const Modal = ({ showModal, setShowModal }) => {
     <>
       {showModal ? <div>
 <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
    <div className="modal-box w-11/12 max-w-5xl">
      <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
      <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
@@ -34,7 +35,7 @@ const Modal = ({ showModal, setShowModal }) => {
      </div>
    </div>
  </div>
- 
+ </div>
 
       
         : null}
