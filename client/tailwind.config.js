e84a0712eecfd3f470d07ee1eac5c0b2907e8da8
James Ball <jamie.j.ball@gmail.com>
2022-07-11 12:56:44 -0700

    added view width

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    sm: '640px',
+    // => @media (min-width: 640px) { ... }
+
+    md: '768px',
+    // => @media (min-width: 768px) { ... }
+
+    lg: '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    xl: '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1440px',
+    // => @media (min-width: 1440px) { ... }
+
+    '3xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
     extend: {},
   },
   daisyui: {
