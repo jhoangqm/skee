914aa9881f7a9d863f0d42c215ae6de9a3dad3a8
James Ball <jamie.j.ball@gmail.com>
2022-07-12 09:12:54 -0700

    update calendar to dynamically block dates

--- client/components/Calendar.tsx
+++ client/components/Calendar.tsx
@@ -5,81 +5,61 @@ import { isWithinInterval } from 'date-fns';
 import useSWR from 'swr';
 import { Bookings } from '@prisma/client';
 
-// const fetcher = (url: string) => fetch(url).then(res => res.json());
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 // fetches all booking dates from the database
 
+function isWithinRange(date, range) {
+  return isWithinInterval(date, { start: range[0], end: range[1] });
+}
 
-const isWithinRange = (date, ranges) => {
-  if (date >= ranges[0] && date <= ranges[1]) {
-    return true;
-  }
-  return false;
-};
-
-
-
-
-// const {data, error} = useSWR<Bookings[]>('/api/bookings', fetcher);
-// if (error) return <div>failed to load</div>;
-// if (!data) return <div>loading...</div>;
-
-
+function isWithinRanges(date, ranges) {
+  return ranges.some(range => isWithinRange(date, range));
+}
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
   const [date, setDate] = useState(new Date());
-  const [disabledRanges, setDisabledRanges] = useState([
-    new Date(2022, 6, 10),
-    new Date(2022, 6, 15),
-  ]);
-  console.log('dis', disabledRanges);
-
-  
-
-  // useEffect(() => {
-  //   setDisabledRanges([[date[0], date[1]]]);
-  // }, [date]);
+  const { data, error } = useSWR<Bookings[]>('/api/bookings/[id]', fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  const ranges = data.map(i => [new Date(i.dateFrom), new Date(i.dateTo)]);
 
   function tileDisabled({ date, view }) {
     // Add class to tiles in month view only
     if (view === 'month') {
       // Check if a date React-Calendar wants to check is within any of the ranges
-      // console.log('date', date);
-      // console.log('disabled', disabledRanges);
-      // console.log('within range',isWithinRange(date, disabledRanges)) 
-      return isWithinRange(date, disabledRanges);
+      console.log(ranges);
+      return isWithinRanges(date, ranges);
     }
   }
-    
-    
 
   const openModal = () => {
     console.log('clicked to openModal');
-    setShowModal(prev => !prev)
-  }
+    setShowModal(prev => !prev);
+  };
 
   return (
     <div className="app">
-      
       <h1 className="text-center">React Calendar</h1>
       <div className="flex justify-center">
         <div htmlFor="my-modal-4" className="calendar-container">
-          
           <Calendar
             onChange={setDate}
             value={date}
             onClickDay={openModal}
             tileDisabled={tileDisabled}
           />
-          <CalMod showModal={showModal} setShowModal={setShowModal} date={date}/>
+          <CalMod
+            showModal={showModal}
+            setShowModal={setShowModal}
+            date={date}
+          />
         </div>
       </div>
-        <p className="text-center">
-          <span className="bold">date selected</span> {date.toDateString()}
-        </p>
-      
+      <p className="text-center">
+        <span className="bold">date selected</span> {date.toDateString()}
+      </p>
     </div>
   );
-};
-
+}
