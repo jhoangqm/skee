eec7f01c575ddc0142b5d466ff8791c7f0751276
James Ball <jamie.j.ball@gmail.com>
2022-07-12 09:52:22 -0700

    add comments make query.id hardcoded

--- client/components/Calendar.tsx
+++ client/components/Calendar.tsx
@@ -1,31 +1,46 @@
 import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import CalMod from './CalendarModal';
+import { useRouter } from 'next/router';
 import { isWithinInterval } from 'date-fns';
 import useSWR from 'swr';
 import { Bookings } from '@prisma/client';
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
-// fetches all booking dates from the database
-
-function isWithinRange(date, range) {
+// * * * * * * * * * * * * * * * *
+// ! this is for getting blocked off dates for pros
+function isWithinRange(date: Date, range: Date[]) {
   return isWithinInterval(date, { start: range[0], end: range[1] });
 }
 
-function isWithinRanges(date, ranges) {
+function isWithinRanges(date: Date, ranges: Date[]) {
   return ranges.some(range => isWithinRange(date, range));
 }
+// * * * * * * * * * * * * * * * *
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
   const [date, setDate] = useState(new Date());
-  const { data, error } = useSWR<Bookings[]>('/api/bookings/[id]', fetcher);
+
+  // * * * * * * * * * * * * * * * *
+  // ! this is for getting blocked off dates for pros
+
+  const { query } = useRouter();
+  // TODO: hard coded query id this need to be dynamic from the modal
+  query.id = '2';
+
+  // fetches booking dates using proId from the database
+  const { data, error } = useSWR<Bookings[]>(
+    () => query.id && `/api/bookings/${query.id}`,
+    fetcher
+  );
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
+
   const ranges = data.map(i => [new Date(i.dateFrom), new Date(i.dateTo)]);
 
-  function tileDisabled({ date, view }) {
+  function tileDisabled({ date, view }: any) {
     // Add class to tiles in month view only
     if (view === 'month') {
       // Check if a date React-Calendar wants to check is within any of the ranges
@@ -33,6 +48,7 @@ export default function Home() {
       return isWithinRanges(date, ranges);
     }
   }
+  // * * * * * * * * * * * * * * * *
 
   const openModal = () => {
     console.log('clicked to openModal');
