165b80f15d737c33df5487e10327a47de0c02082
James Ball <jamie.j.ball@gmail.com>
2022-07-12 09:53:32 -0700

    remove get specific bookings

--- client/pages/api/bookings/index.tsx
+++ client/pages/api/bookings/index.tsx
@@ -1,9 +1,11 @@
 import { prisma } from '../../../db';
-import type { NextApiRequest, NextApiResponse } from 'next'
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  
-  if (req.method === "POST"){
-    const date = JSON.parse(req.body)
+import type { NextApiRequest, NextApiResponse } from 'next';
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const date = JSON.parse(req.body);
     const booking = await prisma.bookings.create({
       data: {
         clientId: 1,
@@ -11,17 +13,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         resortId: 3,
         timeSlotsId: 2,
         dateFrom: date,
-        dateTo: date
-      }
-    })
-  } 
-  if(req.method === "GET"){
-    const bookings = await prisma.bookings.findMany({
-      where: {
-        proId: 1,
-      }
+        dateTo: date,
+      },
     });
+  }
+  if (req.method === 'GET') {
+    const bookings = await prisma.bookings.findMany();
     res.json(bookings);
-    
   }
 }
