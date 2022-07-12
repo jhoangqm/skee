efcfde6a668511d790ea8e8c5042c81f4acc81f8
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-11 17:42:42 -0700

    Co-authored-by: James Ball <Jimmy-b36@users.noreply.github.com>

---
+++ client/pages/api/bookings/index.tsx
@@ -0,0 +1,27 @@
+import { prisma } from '../../../db';
+import type { NextApiRequest, NextApiResponse } from 'next'
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+  if (req.method === "POST"){
+    const date = JSON.parse(req.body)
+    const booking = await prisma.bookings.create({
+      data: {
+        clientId: 1,
+        proId: 2,
+        resortId: 3,
+        timeSlotsId: 2,
+        dateFrom: date,
+        dateTo: date
+      }
+    })
+  } 
+  if(req.method === "GET"){
+    const bookings = await prisma.bookings.findMany({
+      where: {
+        proId: 1,
+      }
+    });
+    res.json(bookings);
+    
+  }
+}
