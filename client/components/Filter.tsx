de7c96c6730b0b1a37e1311f68005866ab11263f
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-12 20:55:03 -0700

    moves instructors into a  components with props

---
+++ client/components/Filter.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+const Filter= () => {
+
+  return (
+  
+    <div className="dropdown">
+    <label tabIndex="0" class="btn m-1">Filter Instructors by</label>
+    <ul tabIndex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+      <li><a>Item 1</a></li>
+      <li><a>Item 2</a></li>
+    </ul>
+  </div>
+  
+  )
+}
+ 
+export default Filter
