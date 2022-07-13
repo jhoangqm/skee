5dbb39201db1add059aeb4c6a924f281b8dd5a7a
James Ball <jamie.j.ball@gmail.com>
2022-07-13 10:13:56 -0700

    re-factor to remove dup[licate code

--- client/pages/api/bookings/index.tsx
+++ client/pages/api/bookings/index.tsx
@@ -11,50 +11,51 @@ const booking = async (id: number, date: any) =>
       dateTo: date,
     },
   });
+const createTimeSlot = async (
+  hours: number,
+  date: Date,
+  bookingDate: string
+) => {
+  const hrs = new Date(date.setHours(hours));
+  const midnight = new Date(date.setHours(-7));
+  const timeSlot = await prisma.timeSlots.upsert({
+    where: {
+      day: midnight.toISOString(),
+    },
+    update: {
+      startTime: hrs,
+      remainingTime: 0,
+      duration: 6,
+    },
+    create: {
+      day: midnight.toISOString(),
+      duration: 3,
+      startTime: hrs.toISOString(),
+      remainingTime: 3,
+    },
+  });
+  booking(timeSlot.id, bookingDate);
+};
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
     let { date, time } = JSON.parse(req.body);
-    const hrsDate = new Date(date);
     const newDate = new Date(date);
     if (time === 'AM') {
-      const hrs = new Date(hrsDate.setHours(2));
-      const midnight = new Date(newDate.setHours(-7));
-      console.log('date', new Date(date));
-      const timeSlot = await prisma.timeSlots.upsert({
-        where: {
-          day: midnight.toISOString(),
-        },
-        update: {
-          remainingTime: 0,
-          duration: 6,
-        },
-        create: {
-          day: midnight.toISOString(),
-          duration: 3,
-          startTime: hrs.toISOString(),
-          remainingTime: 3,
-        },
-      });
-      booking(timeSlot.id, date);
+      createTimeSlot(2, newDate, date);
+    } else if (time === 'PM') {
+      createTimeSlot(6, newDate, date);
     } else {
-      const hrs = new Date(hrsDate.setHours(6));
+      const hrs = new Date(newDate.setHours(2));
       const midnight = new Date(newDate.setHours(-7));
-      const timeSlot = await prisma.timeSlots.upsert({
-        where: {
+      const timeSlot = await prisma.timeSlots.create({
+        data: {
           day: midnight.toISOString(),
-        },
-        update: {
-          remainingTime: 0,
           duration: 6,
-        },
-        create: {
-          day: midnight.toISOString(),
-          duration: 3,
           startTime: hrs.toISOString(),
-          remainingTime: 3,
+          remainingTime: 0,
         },
       });
       booking(timeSlot.id, date);
