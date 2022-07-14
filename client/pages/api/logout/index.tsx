9c970bd26ec5da23cad2862ff0589784f778917f
James Ball <jamie.j.ball@gmail.com>
2022-07-14 11:09:29 -0700

    global cookie destroyer

--- client/pages/api/logout/index.tsx
+++ client/pages/api/logout/index.tsx
@@ -2,10 +2,8 @@ import { withIronSessionApiRoute } from 'iron-session/next';
 
 export default withIronSessionApiRoute(
   function logoutRoute(req: any, res: any) {
-    if (req.session.user.type === 'pro') {
-      req.session.destroy();
-      res.send({ ok: true });
-    }
+    req.session.destroy();
+    res.send({ ok: true });
   },
   {
     cookieName: 'user',
