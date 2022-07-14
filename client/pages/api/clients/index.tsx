181db7cf2cfa2481b0367261e9f89a98aa2712cb
James Ball <jamie.j.ball@gmail.com>
2022-07-14 11:06:28 -0700

    client signup page

---
+++ client/pages/api/clients/index.tsx
@@ -0,0 +1,34 @@
+import { prisma } from '../../../db';
+import { withIronSessionApiRoute } from 'iron-session/next';
+
+export default withIronSessionApiRoute(
+  async function handler(req: any, res: any) {
+    if ((req.method = 'POST')) {
+      const parsed = JSON.parse(req.body);
+      const { firstName, lastName, email, password, phoneNumber } = parsed;
+      try {
+        const client = await prisma.clients.create({
+          data: {
+            firstName,
+            lastName,
+            email,
+            password,
+            phoneNumber,
+          },
+        });
+        req.session.user = {
+          id: client.id,
+          type: 'client',
+        };
+        await req.session.save();
+        res.json(client);
+      } catch (error) {
+        res.json('signup Error', error);
+      }
+    }
+  },
+  {
+    cookieName: 'user',
+    password: 'v85zNiWmcmApJFuUKXDeyW$ShGDJx^7QB%t',
+  }
+);
