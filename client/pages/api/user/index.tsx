beab41a4759d6e4e9d7985efa19b673df48839bc
James Ball <jamie.j.ball@gmail.com>
2022-07-14 11:09:57 -0700

    query to find clients

--- client/pages/api/user/index.tsx
+++ client/pages/api/user/index.tsx
@@ -15,6 +15,17 @@ export default withIronSessionApiRoute(
       } catch (error) {
         res.json('No such session', error);
       }
+    } else if (req.session.user?.type === 'client') {
+      try {
+        const clientSession = await prisma.clients.findMany({
+          where: {
+            id: req.session.user.id,
+          },
+        });
+        res.send(clientSession);
+      } catch (error) {
+        res.json('No such session', error);
+      }
     } else {
       res.json('No such session');
     }
