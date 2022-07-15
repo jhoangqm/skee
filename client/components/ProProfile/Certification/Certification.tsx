4cc4ea6210fb560b490aa2f543df98c6c42f472d
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-15 10:36:10 -0400

    Changes certification component path

--- client/components/ProProfile/Certification/Certification.tsx
+++ client/components/ProProfile/Certification/Certification.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
 
+// This component fetches the certification from DB
 function Certification(props: {proId}) {
   const [certs, setCerts] = useState([])
   
@@ -19,7 +20,7 @@ useEffect(() => fetchCert(),[])
 console.log('certs data: ', certs)
 
   return (
-    <div>Certification</div>
+    <div><img src='https://scontent.fyhu2-1.fna.fbcdn.net/v/t1.18169-9/27544817_1772446526394328_1212537059865333027_n.jpg?stp=cp0_dst-jpg_e15_p240x240_q65&_nc_cat=106&ccb=1-7&_nc_sid=110474&_nc_ohc=Yju2vn4mSccAX9Dahmg&_nc_ht=scontent.fyhu2-1.fna&oh=00_AT8ZRhnULVfkv49BtHIep2Tw0nTamtBhpyCqJ_7N7jmrsA&oe=62F8BB6B'></img></div>
   )
 }
 
