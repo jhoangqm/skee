3c9a9cd450012e4698786b9bbdc54be0eaa7c654
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-14 23:21:13 -0400

    Fetches data from DB to get cert info works but not completed

--- client/components/Profile/Certification/Certification.tsx
+++ client/components/Profile/Certification/Certification.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
 
-function Certification() {
-const [certs, setCerts] = useState([])
+function Certification(props: {proId}) {
+  const [certs, setCerts] = useState([])
+  
+  const { query } = useRouter();
+  query.id = props.proId;
 
-// fetch the cert from the db
-const fetchCert = () => {
-  fetch(`/api/uploads/certification`)
-  .then()
+  // fetch the cert from the db
+  const fetchCert = () => {
+  fetch(`/api/uploads/certification/${props.proId}`)
+  .then(res => res.json())
+  .then(data => setCerts(data))
 }
 
-useEffect(() => {
+useEffect(() => fetchCert(),[])
 
-})
+console.log('certs data: ', certs)
 
   return (
     <div>Certification</div>
