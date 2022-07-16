77b95ba30bb9a9af99f9fd0daf7e00f2d9234772
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-15 23:55:17 -0400

    Adds testing for certification component ( needs upload to work properly)

--- client/components/ProProfile/Certification/Certification.tsx
+++ client/components/ProProfile/Certification/Certification.tsx
@@ -10,19 +10,25 @@ function Certification(props: {proId}) {
 
   // fetch the cert from the db
   const fetchCert = () => {
-  fetch(`/api/uploads/certification/${props.proId}`)
+  fetch(`/api/uploads/certification`)
   .then(res => res.json())
   .then(data => setCerts(data))
 }
 
-useEffect(() => fetchCert(),[])
+useEffect(() => fetchCert() ,[])
+
+const certImage = () => {
+  certs.map(c=>c.certImg)
+}
+
+console.log('Hello from certImage: ', certImage)
 
 console.log('certs data: ', certs)
 
   return (
     <div>
       <h4>Image from server</h4>
-      <img src='https://scontent.fyhu2-1.fna.fbcdn.net/v/t1.18169-9/27544817_1772446526394328_1212537059865333027_n.jpg?stp=cp0_dst-jpg_e15_p240x240_q65&_nc_cat=106&ccb=1-7&_nc_sid=110474&_nc_ohc=Yju2vn4mSccAX9Dahmg&_nc_ht=scontent.fyhu2-1.fna&oh=00_AT8ZRhnULVfkv49BtHIep2Tw0nTamtBhpyCqJ_7N7jmrsA&oe=62F8BB6B'>
+      <img src={certImage}>
         </img>
     </div>
   )
