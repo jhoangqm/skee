fb84ba04de63f152dfdec17c5bde03625ad9df3c
James Ball <jamie.j.ball@gmail.com>
2022-07-17 13:15:30 -0700

    avatar container

--- client/components/Avatar.tsx
+++ client/components/Avatar.tsx
@@ -1,32 +1,35 @@
-import {useEffect, useState} from 'react'
-import UploadAvatar from './Upload/UploadAvatar'
+import { useEffect, useState } from 'react';
+import UploadAvatar from './Upload/UploadAvatar';
 
-
-function Avatar(props: {proId}) {
-  const [avatars, setAvatars] = useState([])
+function Avatar(props: { proId }) {
+  const [avatars, setAvatars] = useState([]);
 
   const fetchAvatars = () => {
     fetch(`/api/uploads/avatar/${props.proId}`)
-    .then(res => res.json())
-    .then(data=> setAvatars(data))
-  }
+      .then(res => res.json())
+      .then(data => setAvatars(data));
+  };
 
-  const prosAvatar = avatars.map(img=>img.image)
+  const prosAvatar = avatars.map(img => img.image);
 
-  console.log('prosAvatars: ', prosAvatar)
+  console.log('prosAvatars: ', prosAvatar);
   /* 
   Will need to fetch the avatar of each user given the id
   Upload avatar to DB via Express and sends data to nextjs api
   */
-  useEffect(()=>fetchAvatars(),[])
+  useEffect(() => fetchAvatars(), []);
 
   return (
     <div>
       <h1>Hello from Avatar</h1>
-      <img src={prosAvatar}></img>
-      <UploadAvatar proId={props.proId}/>
+      <img
+        src={prosAvatar}
+        object-fit="contain"
+        className="flex justify-start bg-blue-200 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+      ></img>
+      <UploadAvatar proId={props.proId} />
     </div>
-  )
+  );
 }
 
-export default Avatar
+export default Avatar;
