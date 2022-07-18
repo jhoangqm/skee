60bff556e160f97869da3b17a8b17feae88c1190
James Ball <jamie.j.ball@gmail.com>
2022-07-17 18:31:36 -0700

    spacing the layout and prettier formatting file

--- client/pages/mountains/index.tsx
+++ client/pages/mountains/index.tsx
@@ -1,7 +1,7 @@
 import Layout from '../../components/Layout';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import styles from "../../styles/Home.module.css";
+import styles from '../../styles/Home.module.css';
 
 const Mountains = () => {
   const [resorts, setResorts] = useState([]);
@@ -17,37 +17,44 @@ const Mountains = () => {
 
   return (
     <Layout>
-      <h1 className={styles.title}>You are looking at all the mountains in Canada</h1>
+      <h1 className={styles.title}>
+        You are looking at all the mountains in Canada
+      </h1>
       <p className={styles.description}>
-            Click on the resort you wish to ski at:
-          </p>
-      <div className="modal-mountains">
-        
-      {resorts.map(resort => (
-        <div key={resort.id} className="flex flex-wrap justify-center  m-5">
-          <div className="card card-side w-100 bg-base-100 shadow-xl">
-            <figure ><img className='object-contain h-20 w-20' src={resort.image} alt="Album" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">{resort.name}</h2>
-              <p>
-                <li>Vertical Meters {resort.vert}</li>
-                <li>Skiable Terrain {resort.skiableTerrain}</li>
-                <li>Number of Runs {resort.runs}</li>
-                <li>Number of lifts {resort.lifts}</li>
-                <li>Green runs {resort.easyRuns}</li>
-                <li>Blue Runs {resort.mediumRuns}</li>
-                <li>Black/Double Black Runs {resort.hardRuns}</li>
-              </p>
-              <div className="card-actions justify-center">
-                <Link href={`/mountains/instructors/${resort.id}`}>
-                  <button className="btn btn-primary">Book an instructor at {resort.name}</button>
-                </Link>   
-           </div>
+        Click on the resort you wish to ski at:
+      </p>
+      <div className="modal-mountains mb-9">
+        {resorts.map(resort => (
+          <div key={resort.id} className="flex flex-wrap justify-center  m-5">
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img src={resort.image} alt="Album" object-fit="contain" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{resort.name}</h2>
+                <p>
+                  <li>Vertical Meters {resort.vert}</li>
+                  <li>Skiable Terrain {resort.skiableTerrain}</li>
+                  <li>Number of Runs {resort.runs}</li>
+                  <li>Number of lifts {resort.lifts}</li>
+                  <li>Green runs {resort.easyRuns}</li>
+                  <li>Blue Runs {resort.mediumRuns}</li>
+                  <li>Black/Double Black Runs {resort.hardRuns}</li>
+                </p>
+                <div className="card-actions justify-center">
+                  <Link href={`/mountains/instructors/${resort.id}`}>
+                    <button className="btn btn-primary w-3/4">
+                      Book an instructor at {resort.name}
+                    </button>
+                  </Link>
+                </div>
               </div>
-            </div></div>
-      ))}</div>
-        </Layout>
-      );
+            </div>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
-      export default Mountains;
+export default Mountains;
