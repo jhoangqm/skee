9dfec215e85aa08ce1058d17b407dd4d385c5688
James Ball <jamie.j.ball@gmail.com>
2022-07-18 14:07:16 -0700

    port number change

--- client/components/Upload/UploadCertification.tsx
+++ client/components/Upload/UploadCertification.tsx
@@ -1,10 +1,8 @@
-import { useState, useRef, useEffect } from "react";
-import axios from "axios";
+import { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
 import { useRouter } from 'next/router';
 
-
-
-function Upload(props: {proId}) {
+function Upload(props: { proId }) {
   // const [image, setImage] = useState("");
   const [file, setFile] = useState();
   const [imagePreview, setPreview] = useState();
@@ -12,61 +10,62 @@ function Upload(props: {proId}) {
 
   const { query } = useRouter();
   query.id = props.proId;
-  
-  const getImage = (e) => {
+
+  const getImage = e => {
     setFile(e.target.files[0]);
-    setPreview(URL.createObjectURL(e.target.files[0]));//shows preview
-  }
-  
+    setPreview(URL.createObjectURL(e.target.files[0])); //shows preview
+  };
+
   //updates image in DB
-  const updateImageDB = (data) =>{
+  const updateImageDB = data => {
     const certObj = {};
-    certObj.uniqueID = props.proId
-    certObj.certImg = data.url
+    certObj.uniqueID = props.proId;
+    certObj.certImg = data.url;
     fetch(`/api/uploads/certification`, {
       method: 'PATCH',
-      body: JSON.stringify(certObj)
-    })
-    .then((res)=>res.json())
-  }
+      body: JSON.stringify(certObj),
+    }).then(res => res.json());
+  };
 
   // uploads image to backend server
-  const uploadImage = (e) => {
-    e.preventDefault()
+  const uploadImage = e => {
+    e.preventDefault();
     const formData = new FormData();
-    formData.append("file", file);
+    formData.append('file', file);
     axios({
-      method: "post",
-      url: "http://localhost:5000/upload",
+      method: 'post',
+      url: 'http://localhost:8000/upload',
       data: formData,
     })
-      .then((response) => {
-      const { data } = response;
-      updateImageDB(data)
-    })
-      .catch((err) => {
-      console.log(err);
-    });
-  }
+      .then(response => {
+        const { data } = response;
+        updateImageDB(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-
-return (
+  return (
     <div className="App">
       <hr></hr>
       {/* <h4>Certification Preview</h4> */}
       <form onSubmit={uploadImage}>
         <div className="imageBox">
-          <img src={imagePreview} width='150px'></img>
+          <img src={imagePreview} width="150px"></img>
         </div>
-        <input type="file"
+        <input
+          type="file"
           onChange={getImage}
-          style={{display: "none"}} //hiding input
+          style={{ display: 'none' }} //hiding input
           ref={inputEl} //set inputEl to referring this element
         ></input>
-        <button className='btn'
-          onClick={() => inputEl.current.click()}
-        >select image</button>
-        <button className='btn' type="submit">upload</button>
+        <button className="btn" onClick={() => inputEl.current.click()}>
+          select image
+        </button>
+        <button className="btn" type="submit">
+          upload
+        </button>
       </form>
     </div>
   );
