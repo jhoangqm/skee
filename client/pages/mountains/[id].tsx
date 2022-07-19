8d1b03475150c93ce73eb64e86ab543e6bf429cc
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-18 17:06:36 -0700

    adds styling for mountai ns page

--- client/pages/mountains/[id].tsx
+++ client/pages/mountains/[id].tsx
@@ -1,14 +1,14 @@
-import Layout from '../../components/Layout';
-import Link from 'next/link';
+import Layout from "../../components/Layout";
+import Link from "next/link";
 
-import Calendar from '../../components/Calendar';
-import styles from '../../styles/Home.module.css';
+import Calendar from "../../components/Calendar";
+import styles from "../../styles/Home.module.css";
 
 export const getStaticPaths = async () => {
-  const res = await fetch('http://localhost:3000/api/resorts/');
+  const res = await fetch("http://localhost:3000/api/resorts/");
   const data = await res.json();
 
-  const paths = data.map(resort => {
+  const paths = data.map((resort) => {
     return {
       params: {
         id: resort.province.toString(),
@@ -21,7 +21,7 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async context => {
+export const getStaticProps = async (context) => {
   const resortId = context.params.id;
   const res = await fetch(`http://localhost:3000/api/resorts/${resortId}`);
   const data = await res.json();
@@ -31,40 +31,89 @@ export const getStaticProps = async context => {
   };
 };
 
+function FrontOfCard({ resort }) {
+  return (
+    <div className="modal-mountains mx-15">
+      <div key={resort.id} className="flex flex-wrap justify-center m-3  ">
+        <div className="card w-96 bg-base-100 shadow-xl m-5 cursor-pointer">
+          <figure>
+            <img src={resort.image} alt="Album" object-fit="contain" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{resort.name}</h2>
+            <p>
+              <li>Vertical Meters {resort.vert}</li>
+              <li>Skiable Terrain {resort.skiableTerrain}</li>
+              <li>Number of Runs {resort.runs}</li>
+              <li>Number of lifts {resort.lifts}</li>
+              <li>Green runs {resort.easyRuns}</li>
+              <li>Blue Runs {resort.mediumRuns}</li>
+              <li>Black/Double Black Runs {resort.hardRuns}</li>
+            </p>
+            <div className="card-actions justify-center">
+              <Link href={`/mountains/instructors/${resort.id}`}>
+                <button className="btn btn-primary w-3/4">
+                  Book an instructor at {resort.name}
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+// hover:scale-110 transition duration-500
+function BackOfCard({ resort }) {
+  return (
+    <div className="modal-mountains mx-15">
+      <div key={resort.id} className="flex flex-wrap justify-center m-3">
+        <div className="card w-96 bg-base-100 shadow-xl m-5 cursor-pointer">
+          <figure>
+            <img src={resort.image} alt="Album" object-fit="contain" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{resort.name}</h2>
+            <p>
+              <li>About the resort {resort.description}</li>
+            </p>
+            <div className="card-actions justify-center">
+              <Link href={`/mountains/instructors/${resort.id}`}>
+                <button className="btn btn-primary w-3/4">
+                  Book an instructor at {resort.name}
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const Mountains = ({ resort }) => {
   return (
     <Layout>
-      <h1 className={styles.title}>
-        Your are looking at all the mountains in {resort[0].province}
-      </h1>
-      <p className={styles.description}>
-        Click on the resort you wish to ski at:
-      </p>
-      <div className="modal-mountains mx-15">
-        {resort.map(resort => (
-          <div key={resort.id} className="flex flex-wrap justify-center">
-            <div className="card w-96 bg-base-100 shadow-xl m-5 ">
-              <figure>
-                <img src={resort.image} alt="Album" object-fit="contain" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{resort.name}</h2>
-                <p>
-                  <li>Vertical Meters {resort.vert}</li>
-                  <li>Skiable Terrain {resort.skiableTerrain}</li>
-                  <li>Number of Runs {resort.runs}</li>
-                  <li>Number of lifts {resort.lifts}</li>
-                  <li>Green runs {resort.easyRuns}</li>
-                  <li>Blue Runs {resort.mediumRuns}</li>
-                  <li>Black/Double Black Runs {resort.hardRuns}</li>
-                </p>
-                <div className="card-actions justify-center">
-                  <Link href={`/mountains/instructors/${resort.id}`}>
-                    <button className="btn btn-primary w-3/4">
-                      Book an instructor at {resort.name}
-                    </button>
-                  </Link>
-                </div>
+      <div className="mt-40">
+        <blockquote className=" text-7xl font-semibold text-center text-slate-900 mt-10 mb-10">
+          Your are looking at all the mountains in&nbsp;
+          <span className=" before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-info relative inline-block">
+            <span className="relative text-white">
+              &nbsp;{resort[0].province}&nbsp;
+            </span>
+          </span>
+        </blockquote>
+      </div>
+
+      <div className=" flex flex-wrap justify-around">
+        {resort.map((resort) => (
+          <div className="flip flex flex-wrap justify-center m-3">
+            <div className="flip-content">
+              <div className="flip-front">
+                <FrontOfCard resort={resort} />
+              </div>
+              <div className="flip-back">
+                <BackOfCard resort={resort} />
               </div>
             </div>
           </div>
